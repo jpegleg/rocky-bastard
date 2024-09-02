97721394536a7c9999d88595f8aabdbe29aeb49c
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-02 18:55:56 -0400

    Update main.rs
    initial functional template for Actix with encrypted PKCS12 and FIPS

--- monolith_template/src/main.rs
+++ monolith_template/src/main.rs
@@ -1,15 +1,49 @@
-use std::collections::HashMap;
-use std::io::{self, BufReader, Read, Write};
-use std::sync::Arc;
-use std::{fs, net, env};
+use actix_web::{web, App, HttpServer, HttpResponse};
+use rustls::{ServerConfig, Certificate, PrivateKey};
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use aws_lc_rs;
+use openssl::pkcs12::Pkcs12;
 
-use docopt::Docopt;
-use log::{debug, error};
+fn load_tls_config() -> Result<ServerConfig, Box<dyn std::error::Error>> {
+    aws_lc_rs::try_fips_mode().expect("Failed to initialize FIPS mode");
 
-use serde::Deserialize;
-use chrono::prelude::*;
-use uuid::Uuid;
+    let pkcs12_path = env::var("PKCSPATH")?;
+    let pkcs12_password = env::var("PKCSPASSWORD")?;
+    let mut file = File::open(pkcs12_path)?;
+    let mut pkcs12_data = vec![];
+    file.read_to_end(&mut pkcs12_data)?;
+    let pkcs12 = Pkcs12::from_der(&pkcs12_data)?;
+    let parsed_pkcs12 = pkcs12.parse2(&pkcs12_password)?;
+    let cert_chain = parsed_pkcs12.cert.into_iter()
+        .map(|cert| Certificate(cert.to_der().unwrap()))
+        .collect::<Vec<_>>();
+    let key = PrivateKey(parsed_pkcs12.pkey.expect("NO PRIVATE KEY FOUND").private_key_to_der().unwrap());
 
-use rustls::crypto::{aws_lc_rs as provider, CryptoProvider};
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(cert_chain, key)?;
 
-fn main () {}
+    Ok(config)
+}
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("OK")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let tls_config = load_tls_config().expect("Failed to load TLS config");
+
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind_rustls("0.0.0.0:7443", tls_config)?
+    .run()
+    .await
+}
