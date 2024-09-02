ff71fbd8c19f168d62bd8e3755d362a30a9e6ac0
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-02 02:16:38 -0400

    Update main.tf
    remove ssh key reference in template

--- tf/main.tf
+++ tf/main.tf
@@ -1,25 +1,17 @@
-resource "vultr_ssh_key" "initkey" {
-  name = "initkey"
-  ssh_key = "public key goes here"
-}
-
 resource "vultr_instance" "r1net" {
     plan = "vc2-1c-1gb"
     region = "atl"
     os_id = 448
-    ssh_key_ids = ["${vultr_ssh_key.initkey.id}"]
 }
 
 resource "vultr_instance" "r2net" {
     plan = "vc2-1c-1gb"
     region = "atl"
     os_id = 448
-    ssh_key_ids = ["${vultr_ssh_key.initkey.id}"]
 }
 
 resource "vultr_instance" "r3net" {
     plan = "vc2-1c-1gb"
     region = "atl"
     os_id = 448
-    ssh_key_ids = ["${vultr_ssh_key.initkey.id}"]
 }
