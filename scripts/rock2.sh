5bdc579a250f351c16f42dd0c4e01dfdb3edc39b
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-04 16:41:22 -0400

    Create rock2.sh
    script for shell part 2 configuration

---
+++ scripts/rock2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+hostname "$1" && sed -i "s/vultr/$1/g" /etc/hosts && echo "$1" > /etc/hostname
+
+fips-mode-setup --check || exit 1
+
+yum update
+yum upgrade
+yum install clamav clamav-update aide -y
+
+aide --init
+freshclam
+
+mkdir -p /opt/monolith/etc /etc/monolith
