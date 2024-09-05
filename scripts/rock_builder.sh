7ab12e91bddaa5ed33995481816ceb481f17c18c
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-04 22:16:45 -0400

    Update rock_builder.sh
    add additional Go source code
    change installed Go version to downgrade to match FIPS version

--- scripts/rock_builder.sh
+++ scripts/rock_builder.sh
@@ -11,17 +11,20 @@ yum update
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - yum upgrade"
 yum upgrade
 
-echo "$(date +%Y-%m-%dT%H:%M:%S)Z - install clamav clamav-update aide gcc clang cmake perl perl-IPC-Cmd openssl"
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - install aide gcc clang cmake perl perl-IPC-Cmd openssl"
 yum install clamav clamav-update aide gcc clang cmake perl perl-IPC-Cmd openssl -y
 
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - run an aide --init"
 aide --init
 
-echo "$(date +%Y-%m-%dT%H:%M:%S)Z - run a freshclam to download latest virus patterns"
-freshclam
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - download and install matching non-FIPS version for FIPS Go 1.22.5"
+wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
 
-echo "$(date +%Y-%m-%dT%H:%M:%S)Z - install Go 1.23.0"
-wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - download Go 1.23.0 as upstream reference"
+wget https://go.dev/dl/go1.23.0.linux-amd64.tar.gz
+
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - download FIPS Go for building/reference"
+wget https://github.com/golang-fips/go/archive/refs/tags/go1.22.5-2-openssl-fips.tar.gz
 
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - install rustup to latest stable cargo"
 rustup update || curl https://sh.rustup.rs -sSf | sh -s -- -y
