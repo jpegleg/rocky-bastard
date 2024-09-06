6ad66cfbb1a530b150b348b8326861315f617f1f
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-06 19:23:25 -0400

    Update rock3.sh
    adding deployment of auditd, kernel sysctl, and kernel grub settings

--- scripts/rock3.sh
+++ scripts/rock3.sh
@@ -13,6 +13,18 @@ read CONTCHECK
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - copy over motd with scp"
 scp files/motd root@"$1":/etc/motd
 
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - backup existing configurations to tarball in root with UTC timestamp"
+ssh root@"$1" "tar czvf /root/$(hostname)_backup_cfg_$(date +%Y-%m-%dT%H:%M:%S)Z.tar.gz /etc/monolith /opt/monolith /usr/local/sbin/monolith /etc/sysctl.conf /etc/default/grub /etc/audit/"
+
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - copy out sysctl.conf file"
+scp files/sysctl.conf root@"$1":/etc/sysctl.conf
+
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - copy out auditd rules base file"
+scp files/audit.rules root@"$1":/etc/audit/rules.d/audit.rules
+
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - copy out auditd rules extended file"
+scp files/extended.rules root@"$1":/etc/audit/rules.d/extended.rules
+
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - copy over monolith app with scp"
 scp files/monolith root@"$1":/usr/local/sbin/monolith
 
@@ -22,11 +34,19 @@ scp files/pki_env root@"$1":/etc/monolith/pki_env
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - copy over identity.p12 app with scp"
 scp files/identity.p12 root@"$1":/opt/monolith/etc/identity.p12
 
-echo "$(date +%Y-%m-%dT%H:%M:%S)Z- copy over unit file for app with scp"
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - copy over unit file for app with scp"
 scp files/monolith.service root@"$1":/etc/systemd/system/monolith.service
 
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - ssh exec set DAC for monolith files"
 ssh root@"$1" "chown root:root /opt/monolith/etc/identity.p12 /etc/monolith/pki_env && chmod 0600 /opt/monolith/etc/identity.p12 /etc/monolith/pki_env"
+
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - make new grub config"
+ssh root@"$1" "grub2-mkconfig"
+
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - reboot to load with new kernel settings"
+ssh root@"$1" "reboot"
+echo "$(date +%Y-%m-%dT%H:%M:%S)Z - sleep for 2 minutes..."
+sleep 120
 
 echo "$(date +%Y-%m-%dT%H:%M:%S)Z - ssh exec set executable monolith, enable, and start the monolith"
 ssh root@"$1" "chmod +x /usr/local/sbin/monolith && systemctl enable monolith && systemctl start monolith"
