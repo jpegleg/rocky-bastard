b57b7384537a0ed9cb934965175b217fbb26089b
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-07 22:06:55 -0400

    Update port_changer.sh
    fix port changer

--- scripts/files/port_changer.sh
+++ scripts/files/port_changer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 semanage port -a -t ssh_port_t -p tcp "$1"
-sed -i "s/$2/$1/g" /etc/ssh/sshd_config
+sed -i "s/Port $2/Port $1/g" /etc/ssh/sshd_config
 grep "^Port $1" /etc/ssh/sshd_config || echo "Port $1" >> /etc/ssh/sshd_config
 firewall-cmd --zone=public --permanent --add-port="$1"/tcp
 firewall-cmd --reload
