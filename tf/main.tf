cde17cf3aa0836ff84eca298410af01c92520f0c
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2024-09-07 21:51:07 -0400

    Update main.tf
    up resource allocation default

--- tf/main.tf
+++ tf/main.tf
@@ -1,17 +1,17 @@
 resource "vultr_instance" "r1net" {
-    plan = "vc2-1c-1gb"
+    plan = "vc2-2c-2gb"
     region = "atl"
     os_id = 448
 }
 
 resource "vultr_instance" "r2net" {
-    plan = "vc2-1c-1gb"
+    plan = "vc2-2c-2gb"
     region = "atl"
     os_id = 448
 }
 
 resource "vultr_instance" "r3net" {
-    plan = "vc2-1c-1gb"
+    plan = "vc2-2c-2gb"
     region = "atl"
     os_id = 448
 }
